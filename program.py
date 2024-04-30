f2a209e9d338926a3bc997fcd91b4e20965b2125
Robert S. - Work Desktop <GitHub@AmaranthAlchemy.com>
2024-04-30 13:10:27 -0700

    Backup

--- program.py
+++ program.py
@@ -2,11 +2,30 @@
 import random
 import sys
 
-def print_paragraphs(n):
-    n = str(n)
-    paragraphs = n.split(" ")
-    num_paragraphs = len(paragraphs)  # Count the number of entries in the list
-    for i in range(num_paragraphs):
-        print(f"Line {i+1}: {paragraphs[i]}")
+# Take a string and print it to the console in a block-text format using an added character as a border, keeping the width of the box to 80 characters
+def print_paragraphs(n,char):
+    width = 80 # Set the width of the box to 80 characters
+    n = str(n) # Convert the input to a string
+    paragraphs = n.split(" ") # Split the string n into a list of strings
+    num_strings = len(paragraphs)  # Count the number of entries in the list
     
-print_paragraphs("test test2 test3 test4")
+    print(char * width) # Print the top border (char) to the console
+    
+    for i in range(num_strings): # Loop through the list
+        char_count = 0 # Initialize the character count
+        line = "" # Initialize the line
+
+        for j in range(len(paragraphs[i])): # Loop through the characters in the string
+            char_count += 1 # Increment the character count
+            line = line + paragraphs[i] # Add the character to the line
+            if char_count == width: # If the character count is equal to the width, break to a new line
+                print(line)
+                i -= 1
+                break
+        print(char, " ") # Print the right border (char) to the console and return to the next line
+            
+    print(char * width) # Print the bottom border (char) to the console
+
+            
+    
+print_paragraphs("test test2 test3 test4", "*")
