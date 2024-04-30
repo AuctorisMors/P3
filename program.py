1716a63050b6517a853a43910902ff7cc5f0f6b2
Robert S. - Work Desktop <GitHub@AmaranthAlchemy.com>
2024-04-30 12:34:29 -0700

    Working Changes

--- program.py
+++ program.py
@@ -1,7 +1,12 @@
 # Description: This is a simple Python program that prints "Hello, World!" to the console, for now.
 import random
+import sys
 
-def main():
-    print(f"Hello, World! {random.randint(0, 100)}")
-
-main()
+def print_paragraphs(n):
+    n = str(n)
+    paragraphs = n.split(" ")
+    num_paragraphs = len(paragraphs)  # Count the number of entries in the list
+    for i in range(num_paragraphs):
+        print(f"Line {i+1}: {paragraphs[i]}")
+    
+print_paragraphs("test test2 test3 test4")
