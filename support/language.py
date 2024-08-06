c8430a54a1fa0e1cb901361e1fb4abf9696da623
Robert S. <GitHub@AmaranthAlchemy.com>
2024-08-05 23:18:18 -0700

    TODO: make settings a full obj.

--- support/language.py
+++ support/language.py
@@ -10,13 +10,13 @@ class Eng:
         'main-menu' : 'Welcome to my humble game browser project. Select from an option below by entering its Number!'
     }
         numbers_game = {
-            'intro': 'Welcome to the Numbers game. Enter a number and Python will try to guess it!',
-            'input': 'Enter a number between 1 and 10,000, with out commas, I will try to guess it using my amazing mathmatical powers!',
-            'isit': 'Is your number ',
-            'guess': 'Is your number higher or lower than ',
-            'guesses': 'guesses: ',
-            'bet': 'How much are you willing to buy-in? For each guess I get wrong, you win 5 percent back!',
-            'win': 'Ah HA! I knew it was '
+        'intro': 'Welcome to the Numbers game. Enter a number and Python will try to guess it!',
+        'input': 'Enter a number between 1 and 10,000, with out commas, I will try to guess it using my amazing mathmatical powers!',
+        'isit': 'Is your number ',
+        'guess': 'Is your number higher or lower than ',
+        'guesses': 'guesses: ',
+        'bet': 'How much are you willing to buy-in? For each guess I get wrong, you win 5 percent back!',
+        'win': 'Ah HA! I knew it was '
         }
 class Esp:
         copy = {
