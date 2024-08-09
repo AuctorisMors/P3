fb922ceb33dc9f99f43f9e24d81fc3e2e6f44020
Robert Silk <GitHub@AmaranthAlchemy.com>
2024-08-09 13:41:09 -0700

    Added spanish and ukrainian lang files finally

--- support/language.py
+++ support/language.py
@@ -1,43 +1,99 @@
 class Eng:
-        copy = {
-            'intro': 'Press Enter to Start! You have: $',
-            'menu1': '3 Lane Slots.',
-            'menu2': 'Number Game.',
-            'menu3': 'Guessing Game.',
-            'request': 'Must be of:',
-            'not-valid': 'Error: Not Valid!',
-            'ending': 'Game over, play again?',
-            'yon' : "Yes or No? $> ",
-            'main-menu' : 'Welcome to my humble game browser project. Select from an option below by entering its Number!',
-            'about_contents': 'This is a little terminal based application that was created in an effort to learn python in a fun and complex manner. The games are not balanced, but point was the code lol.',
-            'about': 'About App.',
-            'exit': 'Quit App.'
-        }
-        numbers_game = {
-            'intro': 'Welcome to the Numbers game. Enter a number and Python will try to guess it!',
-            'input': 'Enter a number between 1 and 10,000, with out commas, I will try to guess it using my amazing mathmatical powers!',
-            'isit': 'Is your number ',
-            'guess': 'Is your number higher or lower than ',
-            'guesses': 'Guesses: ',
-            'bet': 'How much are you willing to buy-in? For each guess I get wrong, you win 5 percent back!',
-            'win': 'Ah HA! I knew it was '
-        }
-        guess_game = {
-            'intro': 'Welcome to the Guessing Game. Can you guess the number Python picked between 1 and 100?',
-            'bet': 'How much are you willing to bet as a buy-in? Each guess you get wrong lowers the amount you win!',
-            'wrong_high': 'Sorry, your guess is too high.',
-            'wrong_low': 'Sorry, your guess is too low.',
-            'guess': 'What number do you guess?',
-            'guesses': 'Guesses: ',
-            'win': 'WOW! You got it!'
-        }
-class Esp:
-        copy = {
+    copy = {
         'intro': 'Press Enter to Start! You have: $',
-        'request': 'Must be of:'
+        'menu1': '3 Lane Slots.',
+        'menu2': 'Number Game.',
+        'menu3': 'Guessing Game.',
+        'request': 'Must be of:',
+        'not-valid': 'Error: Not Valid!',
+        'ending': 'Game over, play again?',
+        'yon' : "Yes or No? $> ",
+        'main-menu' : 'Welcome to my humble game browser project. Select from an option below by entering its Number!',
+        'about_contents': 'This is a little terminal based application that was created in an effort to learn python in a fun and complex manner. The games are not balanced, but point was the code lol.',
+        'about': 'About App.',
+        'exit': 'Quit App.'
+    }
+    numbers_game = {
+        'intro': 'Welcome to the Numbers game. Enter a number and Python will try to guess it!',
+        'input': 'Enter a number between 1 and 10,000, with out commas, I will try to guess it using my amazing mathmatical powers!',
+        'isit': 'Is your number ',
+        'guess': 'Is your number higher or lower than ',
+        'guesses': 'Guesses: ',
+        'bet': 'How much are you willing to buy-in? For each guess I get wrong, you win 5 percent back!',
+        'win': 'Ah HA! I knew it was '
+    }
+    guess_game = {
+        'intro': 'Welcome to the Guessing Game. Can you guess the number Python picked between 1 and 100?',
+        'bet': 'How much are you willing to bet as a buy-in? Each guess you get wrong lowers the amount you win!',
+        'wrong_high': 'Sorry, your guess is too high.',
+        'wrong_low': 'Sorry, your guess is too low.',
+        'guess': 'What number do you guess?',
+        'guesses': 'Guesses: ',
+        'win': 'WOW! You got it!'
+    }
+class Esp:
+    copy = {
+        'intro': '¡Presiona Enter para comenzar! Tienes: $',
+        'menu1': 'Tragamonedas de 3 carriles.',
+        'menu2': 'Juego de números.',
+        'menu3': 'Juego de adivinanzas.',
+        'request': 'Debe ser de:',
+        'not-valid': '¡Error: No válido!',
+        'ending': 'Juego terminado, ¿jugar de nuevo?',
+        'yon': "¿Sí o No? $> ",
+        'main-menu': '¡Bienvenido a mi humilde proyecto de navegador de juegos! Selecciona una opción a continuación ingresando su número.',
+        'about_contents': 'Esta es una pequeña aplicación basada en terminal que fue creada con el fin de aprender Python de una manera divertida y compleja. Los juegos no están equilibrados, pero el punto era el código, jaja.',
+        'about': 'Acerca de la aplicación.',
+        'exit': 'Salir de la aplicación.'
+    }
+    numbers_game = {
+        'intro': '¡Bienvenido al juego de números! ¡Ingresa un número y Python intentará adivinarlo!',
+        'input': 'Ingresa un número entre 1 y 10,000, sin comas, ¡intentaré adivinarlo usando mis increíbles poderes matemáticos!',
+        'isit': '¿Es tu número ',
+        'guess': '¿Es tu número mayor o menor que ',
+        'guesses': 'Adivinanzas: ',
+        'bet': '¿Cuánto estás dispuesto a apostar? ¡Por cada adivinanza incorrecta, ganas un 5 por ciento de vuelta!',
+        'win': '¡Ah HA! Sabía que era '
+    }
+    guess_game = {
+        'intro': '¡Bienvenido al juego de adivinanzas! ¿Puedes adivinar el número que Python eligió entre 1 y 100?',
+        'bet': '¿Cuánto estás dispuesto a apostar como entrada? ¡Cada adivinanza incorrecta reduce la cantidad que ganas!',
+        'wrong_high': 'Lo siento, tu adivinanza es demasiado alta.',
+        'wrong_low': 'Lo siento, tu adivinanza es demasiado baja.',
+        'guess': '¿Qué número adivinas?',
+        'guesses': 'Adivinanzas: ',
+        'win': '¡WOW! ¡Lo lograste!'
     }
 class Ukr:
-        copy = {
-        'intro': 'Press Enter to Start! You have: $',
-        'request': 'Must be of:'
+    copy = {
+        'intro': 'Натисніть Enter, щоб почати! У вас є: $',
+        'menu1': '3-лінійні слоти.',
+        'menu2': 'Гра з числами.',
+        'menu3': 'Гра в здогадки.',
+        'request': 'Повинно бути:',
+        'not-valid': 'Помилка: Неправильно!',
+        'ending': 'Гра закінчена, грати знову?',
+        'yon': "Так чи Ні? $> ",
+        'main-menu': 'Ласкаво просимо до мого скромного проекту браузера ігор. Виберіть опцію нижче, ввівши її номер!',
+        'about_contents': 'Це невеликий додаток на основі терміналу, створений з метою вивчення Python у веселий та складний спосіб. Ігри не збалансовані, але головне був код, хаха.',
+        'about': 'Про додаток.',
+        'exit': 'Вийти з додатку.'
+    }
+    numbers_game = {
+        'intro': 'Ласкаво просимо до гри з числами. Введіть число, і Python спробує його вгадати!',
+        'input': 'Введіть число від 1 до 10,000 без ком, я спробую його вгадати, використовуючи свої дивовижні математичні здібності!',
+        'isit': 'Чи є ваше число ',
+        'guess': 'Чи є ваше число більше або менше ніж ',
+        'guesses': 'Спроби: ',
+        'bet': 'Скільки ви готові поставити? За кожну неправильну спробу ви виграєте 5 відсотків назад!',
+        'win': 'Ах ХА! Я знав, що це '
+    }
+    guess_game = {
+        'intro': 'Ласкаво просимо до гри в здогадки. Чи можете ви вгадати число, яке вибрав Python між 1 і 100?',
+        'bet': 'Скільки ви готові поставити як вхідну ставку? Кожна неправильна спроба зменшує суму, яку ви виграєте!',
+        'wrong_high': 'Вибачте, ваша здогадка занадто висока.',
+        'wrong_low': 'Вибачте, ваша здогадка занадто низька.',
+        'guess': 'Яке число ви вгадуєте?',
+        'guesses': 'Спроби: ',
+        'win': 'ВАУ! Ви вгадали!'
     }
